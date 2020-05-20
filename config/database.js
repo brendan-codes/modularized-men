1e7d332c3049c5d547cf1a4fcc91792b7f89fd0c
Brendan Stanton <bmsenate@gmail.com>
2020-05-20 13:49:01 -0700

    removed extra folders

--- config/database.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-mongoose.connect('mongodb://localhost/dog-db', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useFindAndModify: false
-});
-
-require('./../models/Dog');
